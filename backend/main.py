da1d54f0e29061861133734061a7ae83acf6fd36
ImNotThatPro <duonglonglangson@gmail.com>
2025-09-09 22:55:05 +0700

    Testing discord bot on the so called server v2

--- backend/main.py
+++ backend/main.py
@@ -123,4 +123,4 @@ def login(user: UserLogin, db: Session = Depends(database.get_db)):
     return {'msg': f'Welcome back, {db_user.username}'}
 
 #Testing slave again
-#Testing slave v2 
+#Testing slave v3
